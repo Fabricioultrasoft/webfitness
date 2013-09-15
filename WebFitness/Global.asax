ef4871a8fd5d90eacd85b0101a041a8551c6b04b
arthuraugsten@gmail.com <arthuraugsten@gmail.com@bfe67a47-234c-6c79-77a5-b010ec7f4b6c>
2013-09-15 04:14:02 +0000

    Versão inicial do Sistema. Somente com a estrutura básica do MVC4 criada.

---
+++ WebFitness/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebFitness.MvcApplication" Language="C#" %>
---
+++ WebFitnessLib/class/generated/Mail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace WebFitnessLib
+{
+    partial class Mail
+    {
+        // Configurações do servidor SMTP
+        private static string mailServer = "smpt.google.com";
+        private static Int32 mailServerPort = 995;
+        private static string mailUserName = "username";
+        private static string mailPassword = "password";
+
+        // Variáveis para montar o E-Mail
+        public string mailTo { get; set; }
+        public string[] mailToCollection { get; set; }
+        public string mailFrom { get; set; }
+        public string mailSubject { get; set; }
+        public string mailBody { get; set; }
+        public string mailCc { get; set; }
+        public string[] mailCcCollection { get; set; }
+        public string mailBcc { get; set; }
+        public string[] mailBccCollection { get; set; }
+        public MailPriority mailPriority { get; set; }
+        public string mailAttachment { get; set; }
+        public string[] mailAttachmentCollection { get; set; }
+
+        
+    }
+}
