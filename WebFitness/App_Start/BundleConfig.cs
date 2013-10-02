fd9d64aeeb11ceef2bb93c0be4bb867dc008a32a
arthuraugsten@gmail.com <arthuraugsten@gmail.com@bfe67a47-234c-6c79-77a5-b010ec7f4b6c>
2013-10-02 02:30:11 +0000

    Primeiro requisito implementado. Criado estrutura do layout.

---
+++ WebFitness/App_Start/BundleConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebFitness
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/admin/base/bootstrap-typeahead.js",
+                        "~/Scripts/admin/base/bootstrap.min.js",
+                        "~/Scripts/admin/base/bootstrap-wysiwyg.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/charts").Include(
+                        "~/Scripts/admin/base/excanvas.min.js",
+                        "~/Scripts/admin/base/jquery.flot.js",
+                        "~/Scripts/admin/base/jquery.flot.orderbars.js",
+                        "~/Scripts/admin/base/jquery.flot.pie.js",
+                        "~/Scripts/admin/base/jquery.flot.resize.js",
+                        "~/Scripts/admin/base/jquery.flot.time.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/calendar").Include(
+                        "~/Scripts/admin/base/fullcalendar.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/avocado").Include(
+                        "~/Content/themes/base/avocado.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/css/bootstrap-responsive.css",
+                        "~/Content/themes/base/css/bootstrap.min.css",
+                        "~/Content/themes/base/css/chosen.css",
+                        "~/Content/themes/base/css/fullcalendar.css",
+                        "~/Content/themes/base/css/photoswipe.css",
+                        "~/Content/themes/base/css/prism.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ WebFitness/Areas/Admin/Controllers/TipoAulaController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFitness.Areas.Admin.Controllers
+{
+    public class TipoAulaController : Controller
+    {
+        private WebfitnessDBEntities db = new WebfitnessDBEntities();
+        private String controller = "Tipo Aula";
+        //
+        // GET: /Admin/TipoAula/
+
+        public ActionResult Index()
+        {
+            @ViewBag.Controller = controller;
+            return View(db.TpAula.ToList());
+        }
+
+        //
+        // GET: /Admin/TipoAula/Create
+
+        public ActionResult Create()
+        {
+            @ViewBag.Controller = controller;
+            return View();
+        }
+
+        //
+        // POST: /Admin/TipoAula/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TpAula tpaula)
+        {
+            if (ModelState.IsValid)
+            {
+                tpaula.dtCriacao = DateTime.Now;
+                tpaula.idTpAula = 1;
+                tpaula.status = 1;
+
+                db.TpAula.Add(tpaula);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tpaula);
+        }
+
+        //
+        // GET: /Admin/TipoAula/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            @ViewBag.Controller = controller;
+
+            TpAula tpaula = db.TpAula.Find(id);
+            if (tpaula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tpaula);
+        }
+
+        //
+        // POST: /Admin/TipoAula/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TpAula tpaula)
+        {
+            @ViewBag.Controller = controller;
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(tpaula).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tpaula);
+        }
+
+        //
+        // GET: /Admin/TipoAula/Inactive/5
+
+        public ActionResult Inactive(int id = 0)
+        {
+            @ViewBag.Controller = controller;
+
+            TpAula tpaula = db.TpAula.Find(id);
+            if (tpaula == null)
+            {
+                return HttpNotFound();
+            }
+            tpaula.status = 0;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Admin/TipoAula/Active/5
+        public ActionResult Active(int id = 0)
+        {
+            @ViewBag.Controller = controller;
+
+            TpAula tpaula = db.TpAula.Find(id);
+            if (tpaula == null)
+            {
+                return HttpNotFound();
+            }
+            tpaula.status = 1;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
